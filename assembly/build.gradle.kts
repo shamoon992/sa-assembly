b77778f394133a2360b408fab2874258061d73e7
Shamoon <shamoonahmed1992@gmail.com>
2024-06-13 10:39:32 +0100

    added assembly module

---
+++ assembly/build.gradle.kts
@@ -0,0 +1,21 @@
+plugins {
+    kotlin("jvm")
+}
+
+group = "com.saappcrafters.assembly.plugin"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    testImplementation(kotlin("test"))
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
+kotlin {
+    jvmToolchain(17)
+}
